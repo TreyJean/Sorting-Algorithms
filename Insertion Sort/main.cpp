7159e594a8f9dedcca068e65752393b6ead881c5
Trey Jean-Baptiste <treyjean127@gmail.com>
2020-10-02 15:37:00 -0400

    Added Insertion Sort Algorithm

---
+++ Insertion Sort/main.cpp
@@ -0,0 +1,33 @@
+// Insertion Sort
+// Name: Trey Jean-Baptiste
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	const int size = 10;
+	int data[size] = {2,7,1,4,8,3,0,9,5,6};
+	int temp; int j;
+
+	for(int i=0; i<size; i++) {
+		cout << data[i] << " ";
+	}
+	cout << endl;
+
+	for(int i=1; i<size; i++) {
+		j = i;
+		while(j >= 0 && data[j] < data[j-1]) {
+			temp = data[j];
+			data[j] = data[j - 1];
+			data[j - 1] = temp;
+			j--;
+		}
+	}
+
+	for(int i=0; i<size; i++) {
+		cout << data[i] << " ";
+	}
+	cout << endl;
+	
+	return 0;
+}
